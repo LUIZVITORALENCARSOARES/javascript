94994617891ebd970b7fdb383a71465c23fa8636
Luiz Vitor Alencar Soares <132412014+LUIZVITORALENCARSOARES@users.noreply.github.com>
2023-10-22 17:52:07 -0300

    salvo

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+
+
+
+
+/*
+var c = 1
+while(c <= 2) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+/*
+    do {
+        [blobo]
+    } while (condição)
+*/
